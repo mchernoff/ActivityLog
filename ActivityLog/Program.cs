026ea55f72f9900adb958cf50bddaaf3fe50535d
Matt <chernoff.matthew@gmail.com>
2020-02-08 14:27:30 -0500

    Renamed projects and solution

--- ActivityLog/Program.cs
+++ ActivityLog/Program.cs
@@ -9,7 +9,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MattAppCS
+namespace ActivityLog
 {
     class Program
     {
--- ActivityLog/Properties/AssemblyInfo.cs
+++ ActivityLog/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MattAppCS")]
+[assembly: AssemblyTitle("ActivityLog")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MattAppCS")]
+[assembly: AssemblyProduct("ActivityLog")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
